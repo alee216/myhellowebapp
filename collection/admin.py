9b2ae544aa107a9e77df6e6ca46bca9b242c34a0
A Lee <alee@Alexanders-MBP.fios-router.home>
2017-04-15 10:48:24 -0400

    Added dynamic data

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from collection.models import Profile
+
+class ProfileAdmin(admin.ModelAdmin):
+	model = Profile
+	list_display = ('name', 'description',)
+	prepopulated_fields = {'slug': ('name',)}
 
 # Register your models here.
+
+admin.site.register(Profile, ProfileAdmin)
