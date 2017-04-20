8008f8d5866ddfb136a6b6d4595fda6a3c0c2618
A Lee <alee@Alexanders-MBP.fios-router.home>
2017-04-19 22:31:59 -0400

    Added browse page

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -22,15 +22,23 @@ from django.contrib.auth.views import (
 )
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.views.generic import (TemplateView,
+	RedirectView,
+)
 from collection import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='home'),
 	url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
 	url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+	url(r'^profiles/$', RedirectView.as_view(pattern_name='browse', permanent=True)),
 	url(r'^profiles/(?P<slug>[-\w]+)/$', views.profile_detail, name='profile_detail'),
 	url(r'^profiles/(?P<slug>[-\w]+)/edit/$', views.edit_profile, name='edit_profile'),
+	url(r'^browse/$', RedirectView.as_view(pattern_name='browse', permanent=True)),
+	url(r'^browse/name/$',
+    views.browse_by_name, name='browse'),
+	url(r'^browse/name/(?P<initial>[-\w]+)/$', 
+    views.browse_by_name, name='browse_by_name'),
 	url(r'^accounts/password/reset/$', 
         password_reset,
         {'template_name':
